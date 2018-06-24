8dbe2f0fab97f3dbad7f0d0cc24e320907a24ca8
Venkat <KrishnaChandarlapati@icloud.com>
2018-06-24 12:23:03 -0400

    intial commit

---
+++ lib/krishna_view_tool.rb
@@ -0,0 +1,6 @@
+require "krishna_view_tool/version"
+require "krishna_view_tool/version/renderer"
+
+module KrishnaViewTool
+  # Your code goes here...
+end
---
+++ lib/krishna_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KrishnaViewTool
+  VERSION = "0.1.0"
+end
